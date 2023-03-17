9363d9da462a41a0503fac691b8f3ab7d6d70a01
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-17 13:55:16 +0900

    メールアドレス認証に変更

--- cmd/book-alloc/server.go
+++ cmd/book-alloc/server.go
@@ -2,32 +2,20 @@ package main
 
 import (
 	"book-alloc/api/v1"
+	"book-alloc/internal/middleware"
+	"book-alloc/internal/user"
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"log"
-	"time"
 )
-
-type login struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-}
-
-type User struct {
-	UserName  string
-	FirstName string
-	LastName  string
-}
-
-const identityKey = "id"
 
 func helloHandler(c *gin.Context) {
 	claims := jwt.ExtractClaims(c)
-	user, _ := c.Get(identityKey)
+	u, _ := c.Get(middleware.IdentityKey)
 	c.JSON(200, gin.H{
-		"userID":   claims[identityKey],
-		"userName": user.(*User).UserName,
+		"userID":   claims[middleware.IdentityKey],
+		"userName": u.(*user.User).Email,
 		"text":     "Hello World.",
 	})
 }
@@ -35,7 +23,7 @@ func helloHandler(c *gin.Context) {
 func main() {
 	g := gin.Default()
 
-	authMiddleware, err := setUpAuth()
+	authMiddleware, err := middleware.NewJwtMiddleware()
 	if err != nil {
 		logrus.Error("setUp auth failed: ?", err)
 	}
@@ -59,67 +47,4 @@ func main() {
 	}
 
 	g.Run()
-}
-
-func setUpAuth() (*jwt.GinJWTMiddleware, error) {
-	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-		Realm:       "test zone",
-		Key:         []byte("secret key"),
-		Timeout:     time.Hour,
-		MaxRefresh:  time.Hour,
-		IdentityKey: identityKey,
-		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			if v, ok := data.(*User); ok {
-				return jwt.MapClaims{
-					identityKey: v.UserName,
-				}
-			}
-			return jwt.MapClaims{}
-		},
-		IdentityHandler: func(c *gin.Context) interface{} {
-			claims := jwt.ExtractClaims(c)
-			return &User{
-				UserName: claims[identityKey].(string),
-			}
-		},
-		Authenticator: func(c *gin.Context) (interface{}, error) {
-			var loginVals login
-			if err := c.ShouldBind(&loginVals); err != nil {
-				return "", jwt.ErrMissingLoginValues
-			}
-			userID := loginVals.Username
-			password := loginVals.Password
-
-			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
-				return &User{
-					UserName:  userID,
-					LastName:  "Bo-Yi",
-					FirstName: "Wu",
-				}, nil
-			}
-
-			return nil, jwt.ErrFailedAuthentication
-		},
-		Authorizator: func(data interface{}, c *gin.Context) bool {
-			if v, ok := data.(*User); ok && v.UserName == "admin" {
-				return true
-			}
-
-			return false
-		},
-		Unauthorized: func(c *gin.Context, code int, message string) {
-			c.JSON(code, gin.H{
-				"code":    code,
-				"message": message,
-			})
-		},
-		TokenLookup:   "header: Authorization, query: token, cookie: jwt",
-		TokenHeadName: "Bearer",
-		TimeFunc:      time.Now,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return authMiddleware, nil
 }
---
+++ internal/middleware/JwtMiddleware.go
@@ -0,0 +1,75 @@
+package middleware
+
+import (
+	"book-alloc/internal/user"
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+const IdentityKey = "email"
+
+type login struct {
+	Email    string `form:"email" json:"email" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+func NewJwtMiddleware() (*jwt.GinJWTMiddleware, error) {
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "test zone",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: IdentityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*user.User); ok {
+				return jwt.MapClaims{
+					IdentityKey: v.Email,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &user.User{
+				Email: claims[IdentityKey].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVals login
+			if err := c.ShouldBind(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			email := loginVals.Email
+			password := loginVals.Password
+
+			if email == "admin@example.com" && password == "admin" {
+				return &user.User{
+					Email: email,
+				}, nil
+			}
+
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*user.User); ok && v.Email == "admin@example.com" {
+				return true
+			}
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		TokenLookup:   "header: Authorization, query: token, cookie: jwt",
+		TokenHeadName: "Bearer",
+		TimeFunc:      time.Now,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return authMiddleware, nil
+}
